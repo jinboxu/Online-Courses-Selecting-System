37cc0b7c3607674ceeb49fa8381343d9a87615a5
xujinbo <15889435213m@sina.cn>
2017-05-28 13:43:34 +0800

    删除了多余的几行

--- course_project/modules/teacher.py
+++ course_project/modules/teacher.py
@@ -6,14 +6,3 @@ class Teacher(object):
         self.age = age
         self.salary = salary
         self.class_list = []
-    # def add_class(self, class_name, class_obj):
-    #     self.class_dic[class_name] = class_obj
-    def show_student(self, class_name):
-        print("\033[32;1m班级%S的学生有：%s\033[0m" %(class_name, ','.join(self.class_dic[class_name])))
-    def take_class(self):
-        pass
-    # def change_score(self, new_score):
-
-
-
-
